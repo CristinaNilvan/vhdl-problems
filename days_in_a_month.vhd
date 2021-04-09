056395ea226393d78009ca47b99ee348a6bbd2d3
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-04-09 21:07:09 +0300

    Update days_in_a_month.vhd

--- days_in_a_month.vhd
+++ days_in_a_month.vhd
@@ -18,27 +18,22 @@ architecture COUNTS_DAYS of DAYS_IN_A_MONTH is
 begin
 	
 	month_28_days <= '1' when month = 2 and is_leap = '0'
-						 else '0';
+				else '0';
 	
 	month_29_days <= '1' when month = 2 and is_leap = '1'
-						 else '0'; 
+				else '0'; 
 							 
-	month_30_days <= '1' when month = 9 or 
-							month = 4 or 
-							month = 6 or 
-							month = 11
-						 else '0';
+	month_30_days <= '1' when month = 9 or month = 4 or month = 6 or month = 11
+				else '0';
 							
-	month_31_days <= '1' when month_30_days = '0' and
-							month_28_days = '0' and
-							month_29_days = '0'
-						 else '0';
+	month_31_days <= '1' when month_30_days = '0' and month_28_days = '0' and month_29_days = '0'
+				else '0';
 		
 	number_of_days <= to_unsigned(30, number_of_days'length) when month_30_days = '1' else   
-					to_unsigned(28, number_of_days'length) when month_28_days = '1' else 
-					to_unsigned(29, number_of_days'length) when month_29_days = '1' else 
-					to_unsigned(31, number_of_days'length) when month_31_days = '1' 
-					else to_unsigned(0, number_of_days'length);
+			to_unsigned(28, number_of_days'length) when month_28_days = '1' else 
+			to_unsigned(29, number_of_days'length) when month_29_days = '1' else 
+			to_unsigned(31, number_of_days'length) when month_31_days = '1' 
+			else to_unsigned(0, number_of_days'length);
 	
 end architecture;
 
